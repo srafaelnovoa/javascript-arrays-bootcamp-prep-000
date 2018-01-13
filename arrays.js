1fadcfb7c983702a88a057dca13ef7f825ec8514
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-13 05:39:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars  = ["snickers", "hundred grand", "kitkat", "skittle"]
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array = [element, ...array]
+  return array
+}
