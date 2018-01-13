93eb85bc612e89e0c02cd5d1f6d40d685b3abb91
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-13 05:44:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,14 +4,14 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  array = [element, ...array]
+  array.unshift(element)
   return array
 }
 function addElementToEndOfArray(array, element) {
   return [...array, element]
 }
 function destructivelyAddElementToEndOfArray(array,element) {
-  array = [...array, element]
+  array.push(element)
   return array
 }
 function accessElementInArray(array,index){
